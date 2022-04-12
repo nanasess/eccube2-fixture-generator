87a90d1f7331e2ddd71d5c54f2cf4b916561ecdf
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2022-04-12 10:44:48 +0900

    Add require of GenerateDummyDataCommand

--- bootstrap.php
+++ bootstrap.php
@@ -3,8 +3,8 @@
 use Eccube2\Console\Application;
 
 if (class_exists('\Eccube2\Console\Application')) {
+    require __DIR__.'/src/Command/GenerateDummyDataCommand.php';
     Application::appendConfigPath(__DIR__.'/config');
 }
-
 
 \class_exists('\Eccube2\Tests\Fixture\Generator');
